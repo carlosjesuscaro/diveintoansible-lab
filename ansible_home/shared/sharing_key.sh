412c73e4877bb5232ed7bc4d136d4e174eabacd3
Carlos Jesus Caro <carlosjesuscaro@icloud.com>
2022-03-04 20:34:48 -0500

    feat(ansible): ssh script

---
+++ ansible_home/shared/sharing_key.sh
@@ -0,0 +1,16 @@
+#!/usr/bash
+# Objective: to share the public key from Ubuntu-C to all other hosts
+
+users=(ansible root)
+oss=(ubuntu centos)
+machines=(1 2 3)
+for user in "${users[@]}"
+  do
+    for os in "${oss[@]}"
+      do
+        for instance in "${machines[@]}"
+          do
+            sshpass -f password.txt ssh-copy-id -o StrictHostKeyChecking=no ${user}@${os}${instance}
+          done
+      done
+  done
